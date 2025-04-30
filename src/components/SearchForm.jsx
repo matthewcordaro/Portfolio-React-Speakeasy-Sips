21551d7c5edb4095587d46ae07ca1a62292bf7a8
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 16:34:32 -0400

    feat: Add SearchForm to  Landing component with functionality

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react"
+import Wrapper from "../assets/wrappers/SearchForm"
+import { Form, useNavigation } from "react-router-dom"
 
-function SearchForm() {
+function SearchForm({ searchTerm }) {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === "submitting"
   return (
-    <div>SearchForm</div>
+    <Wrapper>
+      <Form className='form'>
+        <input
+          type='search'
+          name='search'
+          defaultValue={searchTerm}
+          className='form-input'
+        />
+        <button type='submit' className='btn' disabled={isSubmitting}>
+          {isSubmitting ? "Searching" : "submit"}
+        </button>
+      </Form>
+    </Wrapper>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,23 +2,28 @@ import React from "react"
 import { useLoaderData } from "react-router-dom"
 import axios from "axios"
 import CocktailList from "../components/CocktailList"
+import SearchForm from "../components/SearchForm"
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
-export async function loader() {
+export async function loader({ request }) {
+  const url = new URL(request.url)
+
   const commonLetters = ["e", "i", "n", "r", "t", "a", "s", "o", "l", "c"]
   const randomLetter = commonLetters[Math.floor(Math.random() * 10)]
-  const searchTerm = randomLetter
+
+  const searchTerm = url.searchParams.get("search") || randomLetter
   const fullUrl = cocktailSearchUrl + searchTerm
   const response = await axios.get(fullUrl)
   return { drinks: response.data.drinks, searchTerm }
 }
 
 function Landing() {
-  const {drinks, searchTerm} = useLoaderData()
+  const { drinks, searchTerm } = useLoaderData()
   return (
     <>
+      <SearchForm searchTerm={searchTerm} />
       <CocktailList drinks={drinks} />
     </>
   )
