89d2dca6e0655b8544f0fdc72b60823b92396ff2
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 14:46:00 -0400

    feat: enhance CocktailCard component with props for dynamic rendering

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/CocktailCard"
 
-function CocktailCard() {
+function CocktailCard({ id, name, image, info, glass }) {
   return (
-    <div>CocktailCard</div>
+    <Wrapper>
+      <div className='img-container'>
+        <img src={image} alt={name} className='img' />
+      </div>
+      <div className='footer'>
+        <h4>{name}</h4>
+        <h5>{glass}</h5>
+        <p>{info}</p>
+        <Link to={"/cocktail/" + String(id)} className="btn">details</Link>
+      </div>
+    </Wrapper>
   )
 }
 
