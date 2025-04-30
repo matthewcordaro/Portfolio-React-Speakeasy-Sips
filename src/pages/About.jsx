1dfb35fbe3eca85926a658e9417a0d423c81e22f
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 13:32:26 -0400

    Lorem the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,8 @@ import Wrapper from "../assets/wrappers/AboutPage"
 function About() {
   return (
     <Wrapper>
-      <h1>About</h1>
+      <h3>About Us</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur iure tempore animi alias, dolorem non eaque nam beatae natus temporibus! Ullam, quas. Vero tenetur natus itaque cupiditate quam nihil dolorem? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum assumenda debitis perspiciatis nemo officiis dolorem corporis nesciunt rem cumque, quo quasi vel recusandae, repellat suscipit sequi? Consequuntur rem tempora commodi!</p>
     </Wrapper>
   )
 }
