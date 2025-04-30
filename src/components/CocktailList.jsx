beefaa67574b18514dbd8090b07d461ff026a311
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 14:39:23 -0400

    feat: implement CocktailList component with drinks mapping and error handling

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react"
+import Wrapper from "../assets/wrappers/CocktailList"
+import CocktailCard from "./CocktailCard"
 
-function CocktailList() {
+function CocktailList({ drinks }) {
+  if (!drinks) {
+    return <h4 style={{ textAlign: "center" }}>No matching cocktails found</h4>
+  }
+
+  const formattedDrinks = drinks.map((drink) => {
+    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = drink
+    return {
+      id: idDrink,
+      name: strDrink,
+      image: strDrinkThumb,
+      info: strAlcoholic,
+      glass: strGlass,
+    }
+  })
   return (
-    <div>CocktailList</div>
+    <Wrapper>
+      {formattedDrinks.map((drink) => {
+        return <CocktailCard key={drink.id} drink={drink} />
+      })}
+    </Wrapper>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useLoaderData } from "react-router-dom"
 import axios from "axios"
+import CocktailList from "../components/CocktailList"
 
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
@@ -11,13 +12,17 @@ export async function loader() {
   const searchTerm = randomLetter
   const fullUrl = cocktailSearchUrl + searchTerm
   const response = await axios.get(fullUrl)
-  return {drinks: response.data.drinks, searchTerm}
+  return { drinks: response.data.drinks, searchTerm }
 }
 
 function Landing() {
-  const data = useLoaderData()
-  console.log(data)
-  return <div>Landing</div>
+  const {drinks, searchTerm} = useLoaderData()
+  console.log(drinks)
+  return (
+    <>
+      <CocktailList drinks={drinks} />
+    </>
+  )
 }
 
 export default Landing
