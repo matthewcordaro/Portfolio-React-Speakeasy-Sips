5cc64f7e23ddfe7d108adf8ce78c714ff0b9f197
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 15:54:26 -0400

    feat: implement Newsletter form with first name, last name, and email fields

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from "react"
 
 function Newsletter() {
   return (
-    <div>Newsletter</div>
+    <form className='form'>
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        SpeakeasySips Newsletter
+      </h4>
+      <div className='form-row'>
+        {/* first name */}
+        <label htmlFor='firstName' className='form-label'>
+          first name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='firstName'
+          id='firstName'
+          defaultValue='Matthew'
+        />
+        {/* last name */}
+        <label htmlFor='lastName' className='form-label'>
+          last name
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='lastName'
+          id='lastName'
+          defaultValue='Cordaro'
+        />
+        {/* email */}
+        <label htmlFor='email' className='form-label'>
+          email
+        </label>
+        <input
+          type='text'
+          className='form-input'
+          name='email'
+          id='email'
+          defaultValue='example@gmail.com'
+        />
+      </div>
+      <button
+        type='submit'
+        className='btn btn-block'
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
   )
 }
 
