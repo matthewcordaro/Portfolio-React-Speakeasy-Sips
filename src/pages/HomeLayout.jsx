1e5ab4158994556cc406d0429aeb66cc7aa6fdcd
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-30 14:57:46 -0400

    feat: add loading state handling in HomeLayout component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,16 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
 function HomeLayout() {
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === "loading"
   return (
     <>
       <Navbar />
       <section className='page'>
-        <Outlet />
+        {isPageLoading ? <div className='loading' /> : <Outlet />}
       </section>
       <Footer />
     </>
